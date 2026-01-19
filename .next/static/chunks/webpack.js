60901052cd74eb9d9c1f0abf6fd0984d3db848a5
hdemattosdantas <hdemattosdantas@gmail.com>
2026-01-19 15:45:33 -0300

    Fix CombatSystem TypeScript error - remove duplicate export

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fbd4010449071628"; }
+/******/ 		__webpack_require__.h = function() { return "fddfbd61a30cae0b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
